c11ab29238469f960ad34687b84df609f685479d
shans-1983 <shankarsa@gmail.com>
2016-07-28 11:11:44 +0530

    files added
    new files and folder

---
+++ AngMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngMVC.MvcApplication" Language="C#" %>
---
+++ AngMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngMVC.Startup))]
+namespace AngMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
